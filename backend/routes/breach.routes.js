a888d37aff4baf67299e2a5704b4044466d38b44
twinklekhandekar <twinklekhandekar1805@gmail.com>
2025-08-18 11:51:32 +0530

    Update breach.routes.js

--- backend/routes/breach.routes.js
+++ backend/routes/breach.routes.js
@@ -1,9 +1,9 @@
-// backend/routes/breach.routes.js
+
 const express = require("express");
 const { checkEmailBreach } = require("../controllers/breachController");
 const router = express.Router();
 
-// POST /api/breach/check
+
 router.post("/check", checkEmailBreach);
 
 module.exports = router;
