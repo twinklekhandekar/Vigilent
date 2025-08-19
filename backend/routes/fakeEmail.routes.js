be265605add7a9dcb24f2043f3b716ad469dbd36
twinklekhandekar <twinklekhandekar1805@gmail.com>
2025-08-19 13:01:19 +0530

    routes

--- backend/routes/fakeEmail.routes.js
+++ backend/routes/fakeEmail.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { listEmails, generateEmail, deleteEmail } = require('../controllers/fakeEmailController');
+const { listEmails, generateEmail, deleteEmail } = require('../controllers/fakeEmailcontroller');
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.get('/list',authMiddleware, listEmails);
